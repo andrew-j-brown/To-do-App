e4b4cd36ce35cc3ffda6de9702cc4f598dc6fa89
andrew-j-brown <andrewjustinbrown@gmail.com>
2023-10-10 20:49:17 -0500

    Added item priorities + npm boxicons + minimal css

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,34 @@
 import { useState } from "react";
+import "boxicons";
 
 function App() {
-  const [todoList, setTodoList] = useState(["item 1", "item2"]);
-  const [newTodo, setNewTodo] = useState("");
+  const [todoList, setTodoList] = useState([
+    { important: false, title: "about", content: "nice" },
+    { important: true, title: "My really cool note", content: "yeah" },
+  ]);
+  const [itemTitle, setItemTitle] = useState("");
+  const [itemContent, setItemContent] = useState("");
+  const [important, setImportant] = useState(false);
 
-  const handleSetNewTodo = (e) => {
-    setNewTodo(e.target.value);
+  const handleSetItemTitle = (e) => {
+    setItemTitle(e.target.value);
+  };
+
+  const handleSetItemContent = (e) => {
+    setItemContent(e.target.value);
+  };
+
+  const handleSetImportant = () => {
+    setImportant(!important);
   };
 
   const handleSetTodoList = () => {
-    setTodoList([newTodo, ...todoList]);
-    setNewTodo("");
+    setTodoList([
+      { important: important, title: itemTitle, content: itemContent },
+      ...todoList,
+    ]);
+    setItemTitle("");
+    setItemContent("");
   };
 
   const handleDelete = (index) => {
@@ -20,28 +38,54 @@ function App() {
   };
 
   return (
-    <>
-      <div>
-        <label htmlFor="todo-form">New to-do</label>
+    <div className="container">
+      <div className="input-container">
+        <label htmlFor="todo-title">Title</label>
         <input
           type="text"
-          id="todo-form"
-          value={newTodo}
-          onChange={handleSetNewTodo}
+          id="todo-title"
+          value={itemTitle}
+          onChange={handleSetItemTitle}
+          placeholder="Title"
         />
-        <button onClick={handleSetTodoList}>Add Item</button>
+        <label htmlFor="todo-content">Content</label>
+        <input
+          type="text"
+          id="todo-content"
+          value={itemContent}
+          onChange={handleSetItemContent}
+          placeholder="Description"
+        />
+        <div className="button-container">
+          <button id="priority" onClick={handleSetImportant}>
+            {important ? "!" : "..."}
+          </button>
+          <button id="add-button" onClick={handleSetTodoList}>
+            Add Item
+          </button>
+        </div>
       </div>
+      <hr />
       <div>
         <ul>
           {todoList.map((todo, index) => (
             <li key={index}>
-              {todo}
-              <button onClick={() => handleDelete(index)}>X</button>
+              {todo.important && <p className="important">!</p>}
+              <div>
+                <p>{todo.title}</p>
+                <p>{todo.content}</p>
+              </div>
+              <button
+                className="delete-button"
+                onClick={() => handleDelete(index)}
+              >
+                X
+              </button>
             </li>
           ))}
         </ul>
       </div>
-    </>
+    </div>
   );
 }
 
